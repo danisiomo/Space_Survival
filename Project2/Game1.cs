7c06ea39fbb9dcd14aa5f595fa4091985005f30f
Даниил <@DESKTOP-LB9VAI4>
2025-03-31 17:56:55 +0500

    Добавьте файлы проекта.

---
+++ Project2/Game1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _shipTexture;
+        private Texture2D _backgroundTexture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _shipTexture = Content.Load<Texture2D>("ship");
+            _backgroundTexture = Content.Load<Texture2D>("background_1");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_backgroundTexture, Vector2.Zero, Color.White); 
+            _spriteBatch.Draw(_shipTexture, new Vector2(400, 300), Color.White); 
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
